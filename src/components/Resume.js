1a951fe61b83f7a76ea2e3352c65d391892eaa2a
Tommy Boileau <tommybspeed@gmail.com>
2022-11-29 13:43:59 -0800

    added picture of resume

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "./Resume.css";
 import resume from "./tommyboileauresume.pdf";
+import resumephoto from "./TBOILEAU_Resume.png";
 
 function Resume() {
   return (
     <div className="resumeDownload">
       <div className="resumeCard">
         <h3 className="resumeTitle">Resume</h3>
-        <img src={resume} alt="Tommy's resume" className="resumeImg"></img>
+        <img src={resumephoto} alt="Tommy's resume" className="resumeImg"></img>
         <a
           title="Download Resume"
           href={resume}
