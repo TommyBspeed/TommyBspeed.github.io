d8b63d922dfcac75cd0141e200e68ef390eb6113
Tommy Boileau <tommybspeed@gmail.com>
2022-11-29 11:32:58 -0800

    publishing page to github pages

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,9 +1,24 @@
 import React from "react";
+import "./Resume.css";
+import resume from "./tommyboileauresume.pdf";
 
-export default function Resume() {
+function Resume() {
   return (
-    <div className="containerResume">
-      <div>Click to download my resume:</div>
+    <div className="resumeDownload">
+      <div className="resumeCard">
+        <h3 className="resumeTitle">Resume</h3>
+        <img src={resumeClip} alt="Tommy's resume" className="resumeImg"></img>
+        <a
+          title="Download Resume"
+          href={resume}
+          download
+          className="btn downloadBtn"
+        >
+          Click to Download
+        </a>
+      </div>
     </div>
   );
 }
+
+export default Resume;
